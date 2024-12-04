c073d1a1d1df994ee2552bcff3caa53c3551ccde
ALBANTA LD <albanta.leondelgado@gmail.com>
2024-12-04 09:33:42 +0000

    First Commit

---
+++ src/js/component/body.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+
+
+const DeveloperDescription = ({tipoViaje}) => {
+	return (<>
+	<div className="principal_developer_description">
+		<span className="principal_developer_description_name">{tipoViaje}</span>
+	</div>
+	</>)
+}
+
+const DeveloperCard = ({tipoViaje}) => {
+	return (<>
+		<div className="principal_developer_card">
+			<img src="https://thumbs.dreamstime.com/z/esquema-lineal-de-l%C3%ADnea-delgada-del-icono-color-avi%C3%B3n-signo-simple-logotipo-193870116.jpg?w=768"/>
+			<DeveloperDescription tipoViaje={tipoViaje} />
+		</div>
+	
+	</>);
+}
+
+
+const Body = () => {
+	return (<>
+		<div className="principal_body">
+			<h2>Una nueva forma de viajar con guías interactivos</h2>
+			<h3>TU VIAJE PERSONALIZADO</h3>
+			<p>Atrévete a viajar libre con un guía que te asesorará en todo momento, con solo abrir tu móvil tu guía completa, paso a paso</p>
+			<div>
+				<DeveloperCard tipoViaje='VIAJES ESPAÑA' />
+				<DeveloperCard tipoViaje='VIAJES EUROPA' />
+				<DeveloperCard tipoViaje='VIAJES MUNDO' />
+			</div>
+		</div>
+	</>);
+}
+
+export default Body;
